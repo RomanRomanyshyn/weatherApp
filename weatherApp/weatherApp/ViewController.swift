9d6a6167c5fb78daea798c518629418da8e65f1f
Roman Romanyshyn <r.romanishin@gmail.com>
2022-08-08 21:43:15 +0300

    initialize the project

---
+++ weatherApp/weatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Roman Romanyshyn on 08.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
